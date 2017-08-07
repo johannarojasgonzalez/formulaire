39d28586a137e8af251d53813fc525463c2667f9
johanna rojas <johanna.rojas@econocom.com>
2017-08-07 20:05:54 +0200

    initial commit forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
+@Component( {
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
+} )
+export class AppComponent implements OnInit {
+
+    public myForm: FormGroup;
+
+    ngOnInit(): void {
+        this.myForm = new FormGroup( {
+            username: new FormControl( '' ),
+            email: new FormControl( '' ),
+            password: new FormControl( '' )
+        } );
+
+        // pour initialiser le formulaire (tous les clés sont obligatoires)
+        this.myForm.setValue( {
+            username: 'johanna',
+            email: 'johanna@bip.fr',
+            password: '1234'
+        } );
+
+        // pour initialiser le formulaire (tous les clés NE sont PAS obligatoires)
+        this.myForm.patchValue( {
+            username: 'pepita'
+        } )
+    }
+
+    submit(): void {
+        console.log( this.myForm );
+        this.myForm.reset( {
+            email: 'exemple@exemple.fr'
+        } );
+    }
 }
