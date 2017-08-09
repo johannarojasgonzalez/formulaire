9921bff74c984773ba09bcc18f288f3dc780663c
johanna rojas <johanna.rojas@econocom.com>
2017-08-09 20:25:27 +0200

    add validators

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component( {
     selector: 'app-root',
@@ -9,31 +10,72 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class AppComponent implements OnInit {
 
     public myForm: FormGroup;
+    @ViewChild( 'confirm' ) public confirm: ElementRef;
 
     ngOnInit(): void {
         this.myForm = new FormGroup( {
-            username: new FormControl( '' ),
-            email: new FormControl( '' ),
-            password: new FormControl( '' )
-        } );
+            username: new FormControl( '', [Validators.required, this.validator1], this.validatorAsync ),
+            email: new FormControl( '', this.emailValidator ),
+            password: new FormControl( ''),
+            confirmPassword: new FormControl( '')
+            //password: new FormControl( '', this.notMatchPasseword.bind( this ) ) // il est nécessaire d'uitliser le bind pour que le this.confirm match avec le this de la classe
+        } , this.passwordMatch );
 
         // pour initialiser le formulaire (tous les clés sont obligatoires)
         this.myForm.setValue( {
             username: 'johanna',
             email: 'johanna@bip.fr',
-            password: '1234'
+            password: '1234',
+            confirmPassword: ''
         } );
 
         // pour initialiser le formulaire (tous les clés NE sont PAS obligatoires)
         this.myForm.patchValue( {
             username: 'pepita'
-        } )
+        } );
+
+        this.myForm.get( 'username' ).errors // liste de clés avec les erreurs pour le control username
+    }
+
+    validator1( formControl: FormControl ): { [s: string]: boolean } {
+        if ( formControl.value === 'paul' ) {
+            return { isPaul: true };
+        } else {
+            return null;
+        }
+    }
+
+    emailValidator( control: FormControl ): { [s: string]: boolean } {
+        if ( !control.value.match( /^[\w.]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/ ) ) {
+            return { emailValidator: control.value };
+        }
+    }
+
+    validatorAsync( formControl: FormControl ): Promise<any> | Observable<any> {
+        return new Promise(( resolve, reject ) => {
+            setTimeout(() => {
+                resolve( null );
+            }, 30000 );
+        } );
+    }
+
+    notMatchPasseword( control: FormControl ): { [s: string]: boolean } {
+        if ( control.value != this.confirm.nativeElement.value ) {
+            return { notMatch: true };
+        }
+    }
+
+    passwordMatch( group: FormGroup ): { [s: string]: boolean } {
+        if ( group.get( 'password' ).value != group.get( 'confirmPassword' ).value ) {
+            console.log("password not match")
+            return { notMatch: true };
+        }
     }
 
     submit(): void {
         console.log( this.myForm );
-        this.myForm.reset( {
-            email: 'exemple@exemple.fr'
-        } );
+        //        this.myForm.reset( {
+        //            email: 'exemple@exemple.fr'
+        //        } );
     }
 }
